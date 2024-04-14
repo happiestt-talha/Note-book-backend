d59f55267c90e58ae9eafe439d6d9251673f9523
Talha <mtalhamanzoor786@gmail.com>
2024-04-14 09:25:36 +0500

    Every single error is handled successfully, no problem, multiple notes being posts from one single id

--- routes/notes.js
+++ routes/notes.js
@@ -5,24 +5,27 @@ const fetchUser = require('../middlewares/fetchUser');
 const Note = require('../models/Note');
 
 // GET /api/notes
-router.get('/', fetchUser, async (req, res) => {
+router.get('/getnote', fetchUser, async (req, res) => {
     try {
+        // res.json({reqUser:req.user})
         const userNotes = await Note.find({ user: req.user._id });
-        res.json(userNotes);
+        res.json({reqUser:req.user,userNotes});
+        // res.json(userNotes);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
 
+
 // POST /api/notes
-router.post('/', fetchUser, [
+router.post('/addnote', fetchUser, [
     body('title').notEmpty().withMessage('Title is required'),
     body('description').notEmpty().withMessage('Description is required')
 ], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
-
+    // res.json({reqUser:req.user})
     const { title, description, tag } = req.body;
     try {
         const newNote = new Note({
@@ -32,7 +35,8 @@ router.post('/', fetchUser, [
             tag
         });
         const savedNote = await newNote.save();
-        res.json(savedNote);
+        res.json({reqUser:req.user,savedNote});
+        // res.json(savedNote);
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
