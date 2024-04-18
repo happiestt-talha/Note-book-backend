f06a9a42570ba9071e468a43259c07c0484d7f92
Talha <mtalhamanzoor786@gmail.com>
2024-04-18 19:35:20 +0500

    Added module to update an existing note

--- routes/notes.js
+++ routes/notes.js
@@ -43,4 +43,33 @@ router.post('/addnote', fetchUser, [
     }
 });
 
+
+router.put('/updatenote/:id', fetchUser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        const newNote = {}
+
+        if (title) { newNote.title = title }
+        if (description) { newNote.description = description }
+        if (tag) { newNote.tag = tag }
+
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send('Not Found');
+        }
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send('Not Allowed');
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+
 module.exports = router;
